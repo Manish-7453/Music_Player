21008ee640b330ca79662399d2b127285a167dc1
Manish-7453 <kiran2811p@gmail.com>
2023-10-29 09:41:37 +0530

    First Commit-files added

--- window_3.py
+++ window_3.py
@@ -11,7 +11,7 @@ import window_4 as mw4
 
 def main_win3():
     root = Tk()
-    root.geometry("1920x1080")
+    root.geometry("880x420")
     root.title("Eternal Music")
     root.configure(background="#000000")
 
@@ -118,11 +118,11 @@ def main_win3():
 
     mixer.init()
 
-    pause=Image.open("C:\\Users\\ASUS\\Downloads\\Programming\\Python_Project\\play.png")
+    pause=Image.open("C:\\Users\\PRANALI PATIL\\Documents\\Music_Player\\play.png")
     pause = pause.resize((80,80),Image.ANTIALIAS)
     pause = ImageTk.PhotoImage(pause)
 
-    head=Image.open("C:\\Users\\ASUS\\Downloads\\Programming\\Python_Project\\top.png")
+    head=Image.open("C:\\Users\\PRANALI PATIL\\Documents\\Music_Player\\top.png")
     head = head.resize((1530,65),Image.ANTIALIAS)
     head= ImageTk.PhotoImage(head)
     headname=Label(root,image=head,bd=0)
@@ -132,60 +132,6 @@ def main_win3():
     frame_img.place(x=18,y=84)
     frame_img.config(background="#627296")
 
-    img1 = ImageTk.PhotoImage(file=f'C:\\Users\\ASUS\\Downloads\\Programming\\Python_Project\\Images\\3.jpg')
-    img2 = ImageTk.PhotoImage(file=f'C:\\Users\\ASUS\\Downloads\\Programming\\Python_Project\\Images\\2.jpg')
-    img3 = ImageTk.PhotoImage(file=f'C:\\Users\\ASUS\\Downloads\\Programming\\Python_Project\\Images\\3.jpg')
-    img = Label(image=img1,bg="black",height=480, width=875)
-    img.place(x=28,y=91)
-
-
-
-    down= Frame(root, relief=SUNKEN, height= 170, width=900)
-    down.place(x=18,y=603)
-    down.config(background="#627296")
-    down.bind('<Double-1>',move_to_win4)   
-
-
-    play=Image.open("C:\\Users\\ASUS\\Downloads\\Programming\\Python_Project\\pause.png")
-    play = play.resize((80,80),Image.ANTIALIAS)
-    play =ImageTk.PhotoImage(play)
-    playbtn=Button(root,image = play,bd=0,command=pause_unpause)
-    playbtn.place(x=425,y=643)
-
-
-    next=Image.open("C:\\Users\\ASUS\\Downloads\\Programming\\Python_Project\\next.png")
-    next = next.resize((80,80),Image.ANTIALIAS)
-    next = ImageTk.PhotoImage(next)
-    nextbtn=Button(root,image = next,bd=0,command=next_song)
-    nextbtn.place(x=553,y=643)
-
-    previous=Image.open("C:\\Users\\ASUS\\Downloads\\Programming\\Python_Project\\previous.png")
-    previous = previous.resize((80,80),Image.ANTIALIAS)
-    previous = ImageTk.PhotoImage(previous)
-    previousbtn=Button(root,image = previous,bd=0,command= previous_song)
-    previousbtn.place(x=297,y=644)
- 
-
-    loadbtn =Button(root,text="Load Music",bg="#4539ed",padx=8,pady=4,fg="white",font="Helvetica 13 bold",bd=0,command=add_song)
-    loadbtn.place(x=1373,y=101)
-
-    my_music = PhotoImage(file="C:\\Users\\ASUS\\Downloads\\Programming\\Python_Project\\mlabel.png")
-    mlabel=Label(image = my_music,bd=0)
-    mlabel.place(x=954,y=87)
-
-
-    play_frame = Frame(root, height =600, width = 578, background ="#9ba4e8",bd=0)
-    play_frame.place(x=938,y=169)
-
-    scroll =Scrollbar(play_frame)
-    playlist = Listbox(play_frame, height = 25, width = 50, bg ="#000000", font = "Helvetica ",fg = "white",yscrollcommand=scroll.set)
-    scroll.config(command=playlist.yview)
-    scroll.pack(side=RIGHT,fill=Y)
-    playlist.pack(fill=BOTH)
-    playlist.bind('<Double-1>',play_song)   
-
-    
-    open_folder()
     
     root.mainloop()
     
